97f5ca3a9a451d7e6256fdd59f28fc1ebd563aec
Susant Sahani <ssahani@redhat.com>
2026-01-17 16:04:01 +0530

    chore: Bump version to 0.0.3 for PyPI release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyper2kvm",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[l.strip() for l in open("requirements.txt", encoding="utf-8") if l.strip() and not l.startswith("#")],
     entry_points={"console_scripts": ["hyper2kvm=hyper2kvm.__main__:main"]},
